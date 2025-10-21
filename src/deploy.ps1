dfc22aec19a51773c305f47823f23478784103e5
DESKTOP-V8DLB94\kobay <kobayashiryu123@gmail.com>
2025-10-21 11:59:12 +0900

    EB環境更新　バッチファイル修正

--- src/deploy.ps1
+++ src/deploy.ps1
@@ -1,12 +1,9 @@
-Set-Location -Path $PSScriptRoot
 cd LolHolmes.Core
-dotnet publish -o "$PSScriptRoot/site"
-cd $PSScriptRoot/site
-Compress-Archive -Path * -DestinationPath ../site.zip -Force
-aws sso login --profile AdministratorAccess-482903906816 
-cd ..
-eb init -p iis lolholmes --region ap-northeast-1 --profile AdministratorAccess-482903906816
-eb deploy
-# åˆå›ã®ã¿
-# eb create lolholmes-env
-eb open
+
+dotnet publish -c Release -o ./publish
+cd publish
+
+# ƒfƒvƒƒC
+eb deploy --profile lolholmes-prof
+
+Write-Host "Deployment script finished."
